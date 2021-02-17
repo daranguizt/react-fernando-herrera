e25f7865d603e8d93671924b27ff6197d74a806c
Diego Aranguiz <diegoaranguizt@gmail.com>
2021-02-17 01:48:35 -0300

    submodule

---
+++ section3.0-jsmoderno/intro-javascript/src/index.js
@@ -0,0 +1,27 @@
+
+const persona = {
+    nombre: 'Tony',
+    apellido: 'Stark',
+    edad: 45,
+    direccion: {
+        ciudad: 'New York',
+        zip: 123123123,
+        lat: 14.1231,
+        lng: 32.92392
+    }
+};
+
+//desde ES5, obviar el nombre es lo mismo que hacer persona: persona
+console.log({persona});
+
+//se copia el puntero 
+// const persona2 = persona;
+// console.log(persona2);
+
+const persona2 = {...persona};
+persona2.nombre = 'Peter';
+
+console.log(persona);
+console.log(persona2);
+
+//asdfasdf
