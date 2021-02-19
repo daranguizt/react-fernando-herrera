b4513c1c46e670c75efe4b4e6e6d71a7ac6e41a0
Diego Aranguiz <diegoaranguizt@gmail.com>
2021-02-19 02:45:04 -0300

    sect4 terminada

--- section4/counter-app/src/CounterApp.js
+++ section4/counter-app/src/CounterApp.js
@@ -1,19 +1,28 @@
 import React, { Fragment, useState } from "react";
 import PropTypes from "prop-types";
 
-const CounterApp = ({ value }) => {
+const CounterApp = ({ value = 10 }) => {
+  const [counter, setCounter] = useState(value);
 
-    const []
-
-  const handleAdd = (e) => {
-    console.log(e);
+  const handleAdd = () => {
+    setCounter(counter + 1);
   };
+
+  const handleReset = () => {
+      setCounter(value);
+  }
+
+  const handleSubstract = () => {
+    setCounter(counter - 1);
+  }
 
   return (
     <Fragment>
       <h1>CounterApp</h1>
-      <h2>{value}</h2>
-      <button onClick={handleAdd}>+1</button>
+      <h2>{counter}</h2>
+      <button onClick={handleAdd}>+1</button> 
+      <button onClick={handleReset}>Reset</button> 
+      <button onClick={handleSubstract}>-1</button> 
     </Fragment>
   );
 };
