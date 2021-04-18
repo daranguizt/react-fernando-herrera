75fecf42c400338d8964a9ac0f5a3d464537a439
Diego Aranguiz <diegoaranguizt@gmail.com>
2021-04-18 04:22:29 -0400

    login and register styles

--- section18-journal_app/journal-app/src/components/auth/LoginScreen.js
+++ section18-journal_app/journal-app/src/components/auth/LoginScreen.js
@@ -1,19 +1,50 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const LoginScreen = () => {
   return (
     <>
-      <h3>Login</h3>
+      <h3 className="auth__title">Login</h3>
 
       <form>
-        <input type="text" placeholder="email" name="email" />
+        <input
+          type="text"
+          placeholder="email"
+          name="email"
+          className="auth__input"
+          autoComplete="off"
+        />
 
-        <innput type="password" placeholder="password" name="password" />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          className="auth__input"
+          autoComplete="off"
+        />
 
-        <button type="submit">Login</button>
+        <button className="btn btn-primary btn-block" type="submit" disabled={false}>
+          Login
+        </button>
 
         <hr />
-        Google
+        <div className="auth__social-networks">
+          <p>Login in with social networks</p>
+          <div className="google-btn">
+            <div className="google-icon-wrapper">
+              <img
+                className="google-icon"
+                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                alt="google button"
+              />
+            </div>
+            <p className="btn-text">
+              <b>Sign in with google</b>
+            </p>
+          </div>
+        </div>
+
+        <Link to="/auth/register" className="link">Create new account</Link>
       </form>
     </>
   );
--- section18-journal_app/journal-app/src/components/auth/RegisterScreen.js
+++ section18-journal_app/journal-app/src/components/auth/RegisterScreen.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const RegisterScreen = () => {
-    return (
-        <div>
-             <h1>Register Screen</h1>
-        </div>
-    )
-}
+  return (
+    <>
+      <h3 className="auth__title">Register</h3>
+
+      <form>
+        <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          className="auth__input"
+          autoComplete="off"
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          name="email"
+          className="auth__input"
+          autoComplete="off"
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          className="auth__input"
+          autoComplete="off"
+        />
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          name="confirm-password"
+          className="auth__input"
+          autoComplete="off"
+        />
+
+        <button
+          className="btn btn-primary btn-block mb-5"
+          type="submit"
+          disabled={false}
+        >
+          Login
+        </button>
+
+        <Link to="/auth/login" className="link">Already registered?</Link>
+      </form>
+    </>
+  );
+};
