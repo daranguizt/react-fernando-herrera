c3cfff7fe906271873298a36dd0a663186397818
Diego Aranguiz <diegoaranguizt@gmail.com>
2021-04-22 23:37:08 -0400

    update screen

--- section18-journal_app/journal-app/src/components/journal/JournalEntry.js
+++ section18-journal_app/journal-app/src/components/journal/JournalEntry.js
@@ -1,27 +1,44 @@
 import React from "react";
+import moment from "moment";
+import { useDispatch } from "react-redux";
+import { setActiveNote } from "../../actions/notes";
 
-export const JournalEntry = () => {
+export const JournalEntry = ({ id, title, body, date, url }) => {
+  const noteDate = moment(date);
+
+  const dispatch = useDispatch();
+  const handleActiveNote = () => {
+    dispatch(
+      setActiveNote(id, {
+        title,
+        body,
+        date,
+        url,
+      })
+    );
+  };
+
   return (
-    <div className="journal__entry pointer">
-      <div
-        className="journal__entry-picture"
-        style={{
-          backgroundSize: "cover",
-          backgroundImage:
-            'url("https://s3-us-west-2.amazonaws.com/melingoimages/Images/91138.jpg")',
-        }}
-      ></div>
+    <div className="journal__entry pointer" onClick={handleActiveNote}>
+      {url && (
+        <div
+          className="journal__entry-picture"
+          style={{
+            backgroundSize: "cover",
+            backgroundImage:
+              'url("https://s3-us-west-2.amazonaws.com/melingoimages/Images/91138.jpg")',
+          }}
+        ></div>
+      )}
 
       <div className="journal__entry-body">
-        <p className="journal__entry-title">It's a new day</p>
-        <p className="journal__entry-content">
-          Why use lorem ipsum if you can write anything
-        </p>
+        <p className="journal__entry-title">{title}</p>
+        <p className="journal__entry-content">{body}</p>
       </div>
 
       <div className="journal__entry-date-box">
-        <span>Monday</span>
-        <h4>2B</h4>
+        <span>{noteDate.format("dddd")}</span>
+        <h4>{noteDate.format("d")}</h4>
       </div>
     </div>
   );
--- section18-journal_app/journal-app/src/components/notes/NoteScreen.js
+++ section18-journal_app/journal-app/src/components/notes/NoteScreen.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useForm } from "../../hooks/useForm";
 import { NotesAppBar } from "./NotesAppBar";
 
 export const NoteScreen = () => {
+  const { active: note } = useSelector((state) => state.notes);
+  const [formValues, handleInputChange, reset] = useForm(note);
+  const { title, body, url } = formValues;
+
+  const activeId = useRef(note.id);
+
+  useEffect(() => {
+    if (note.id !== activeId.current) {
+      reset(note);
+    }
+  }, [reset, note]);
+
   return (
     <div className="notes__main-content">
       <NotesAppBar />
@@ -12,19 +26,23 @@ export const NoteScreen = () => {
           placeholder="Some awesome title"
           className="notes__title-input"
           autoComplete="off"
+          name="title"
+          onChange={handleInputChange}
+          value={title}
         />
 
         <textarea
           placeholder="what happened today"
           className="notes__textarea"
+          onChange={handleInputChange}
+          value={body}
         ></textarea>
 
-        <div className="notes__images">
-          <img
-            src="https://pbs.twimg.com/media/Dmx9R_TXgAERFG3.jpg:large"
-            alt="fmab landscape"
-          />
-        </div>
+        {url && (
+          <div className="notes__images">
+            <img src={url} alt={title} />
+          </div>
+        )}
       </div>
     </div>
   );
